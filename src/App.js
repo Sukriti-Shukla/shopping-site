8567e78d93fd94955d2d22171991739833c37322
Sukriti Shukla <sukritishukla0507@gmail.com>
2022-05-25 12:21:39 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+//import Items from "./components/Items";
+import Cart from "./components/Cart";
 
 function App() {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
+
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
+
+  // return (
+  //   <React.Fragment>
+  //     <Navbar setShow={setShow} size={cart.length} />
+  //     {show ? (
+  //       <Dashboard handleClick={handleClick} />
+  //     ) : (
+  //       <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+  //     )}
+  //   </React.Fragment>
+  // );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar setShow={setShow} size={cart.length} />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Dashboard handleClick={handleClick} />} />
+
+          <Route
+            path="/cart"
+            element={
+              <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
